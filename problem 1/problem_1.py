c8581d63d0d5fc3e6ae3b27c0cf2e9f5e3b46e6e
50566348 <50566348@CW-B207-1228S>
2023-11-21 16:24:05 +0000

    Add project files.

---
+++ problem 1/problem_1.py
@@ -0,0 +1,10 @@
+def convert_temperature(temperature, method):
+    if method == "C_to_F":
+        convert = temperature * 1.8 + 32
+    elif method == "f_to_C":
+        convert = temperature - 32 * (5/9)
+        return convert
+
+print(convert_temperature(32, "C_to_F")
+print(convert_temperature(100, "C_to_F")
+
